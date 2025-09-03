51ed2e3e4ea9cb6c95b38e4e39e7cf7452e6ce58
Sophie ARNAUD <soso.arnaud26@gmail.com>
2025-09-03 16:22:44 +0100

    refonte de la premiere map

---
+++ tileset/decorative_cracks_coasts_animation.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="decorative_cracks_coasts_animation" tilewidth="16" tileheight="16" tilecount="475" columns="19">
+ <image source="decorative_cracks_coasts_animation.png" width="304" height="400"/>
+</tileset>
