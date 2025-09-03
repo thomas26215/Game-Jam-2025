873bb680007a6b39df044525bec3902ffb13bed7
brayan <bilsbrayan@gmail.com>
2025-09-03 19:14:59 +0200

    ajout map left_top

--- tileset/michael-howe-stonewallwithcolumns-tileset-002.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="michael-howe-stonewallwithcolumns-tileset-002" tilewidth="16" tileheight="16" tilecount="6636" columns="79">
- <image source="michael-howe-stonewallwithcolumns-tileset-002.jpg" width="1272" height="1345"/>
-</tileset>
