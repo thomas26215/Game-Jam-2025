53c0e5fc5c14a37139f0e0a86478b2e538e11295
brayan <bilsbrayan@gmail.com>
2025-09-03 19:29:27 +0200

    passage des png en tsx

---
+++ tileset/Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Objects" tilewidth="16" tileheight="16" tilecount="216" columns="24">
+ <image source="Objects.png" width="384" height="144"/>
+</tileset>
---
+++ tileset/Water_coasts_animation.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Water_coasts_animation" tilewidth="16" tileheight="16" tilecount="928" columns="29">
+ <image source="Water_coasts_animation.png" width="464" height="512"/>
+</tileset>
