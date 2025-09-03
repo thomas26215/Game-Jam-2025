9b46ae6439976989ab6358e7204a640471003b4c
Sophie ARNAUD <soso.arnaud26@gmail.com>
2025-09-03 16:07:39 +0100

    ajout de tileset

---
+++ maps/Dungeon_Autotiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Dungeon_Autotiles" tilewidth="16" tileheight="16" tilecount="300" columns="20">
+ <image source="../tileset/Dungeon_Autotiles.png" width="320" height="240"/>
+</tileset>
---
+++ maps/overworld_tileset_grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="overworld_tileset_grass" tilewidth="16" tileheight="16" tilecount="252" columns="12">
+ <image source="../tileset/overworld_tileset_grass.png" width="192" height="336"/>
+</tileset>
